b9ea58ad30c99cf8beb92d53499fd597d31b80bf
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-05-26 14:39:39 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e # give an error if any command finishes with a non-zero exit code
+set -u # give an error if we reference unset variables
+set -o pipefail # for a pipeline, if any of the commands fail with a non-zero exit code, fail the entire pipeline with that exit code
+
+
+mkdir simpmsg
+cp simpmsg.py simpmsg/__init__.py
+
+#python setup.py sdist bdist_wheel
