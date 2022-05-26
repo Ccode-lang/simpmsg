5734abf84e02b6c3b201c1b8d622a2cfbfcf8079
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-05-26 14:34:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpmsg", # Replace with your own username
+    version="0.0.1",
+    author="Ccode-lang",
+    author_email="coop112097@gmail.com",
+    description="For all the people too lazy too learn to use tcp.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ccode-lang/simpmsg",
+    packages=setuptools.find_packages(),
+    install_requires = [],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
