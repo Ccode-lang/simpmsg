a189bd749a071d9916890e2f9db2912477e34b09
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-07-25 18:10:42 -0700

    Update simpmsg.py

--- simpmsg.py
+++ simpmsg.py
@@ -21,6 +21,7 @@ class simpserve(object):
             raise OSError("Server never started, can't reacive message")
     def sendmsg(self, msg, host, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
         s.connect((host,port))
         s.send(msg.encode())
         s.close()
